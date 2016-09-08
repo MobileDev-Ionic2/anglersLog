b1ecb15e7522d65c02a7c226cb8d03d50f0566c0
Ebbe Vang <ev@evang.dk>
2016-09-08 09:49:35 +0200

    small changes?

--- app/pages/contact/contact.ts
+++ app/pages/contact/contact.ts
@@ -1,10 +1,51 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'build/pages/contact/contact.html'
 })
 export class ContactPage {
-  constructor(private navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+    
+  }
+
+ 
+
+  public coolFeatures:boolean;
+
+  coolFeaturesChanged()
+  {
+    console.log("changed col features toggle: "+this.coolFeatures);
+    
+    if (this.coolFeatures == false)
+    {
+      this.showDialog();
+    }
+  }
+
+  showDialog()
+  {
+    let alert = this.alertCtrl.create({
+    title: 'Disable Cool Features?',
+    message: 'Do you really want to disable the cool features',
+    buttons: [
+      {
+        text: 'No',
+        role: 'no',
+        handler: () => {
+          console.log('No clicked');
+          this.coolFeatures = true;
+        }
+      },
+      {
+        text: 'Yes',
+        handler: () => {
+          console.log('Yes clicked');
+        }
+      }
+    ]
+    });
+    alert.present();
   }
 }
